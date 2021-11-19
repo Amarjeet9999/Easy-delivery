b73c12c2f3f38513ffff3380c4a3c521ef615962
Amaarjeet Kumar <amarjeetyt@gmail.com>
2021-11-19 17:37:58 +0530

    Backend Structure Ready

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,12 @@
 import "./App.css";
 import { NavContainer } from "./Components/Navbar/NavContainer";
+import { LoginPage } from "./Pages/LoginPage/LoginPage";
 
 function App() {
   return (
     <div className="App">
       <NavContainer page={"landing"} />
+      <LoginPage />
     </div>
   );
 }
