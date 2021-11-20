ca3b0d251a8fdea7dd8e0ceb3f0db10d50cbdf33
sarangKaliyath <86410007+sarangKaliyath@users.noreply.github.com>
2021-11-20 18:44:36 +0530

    Driver Jobs update

--- frontend/src/Pages/DriverDashboard/DriverJob.jsx
+++ frontend/src/Pages/DriverDashboard/DriverJob.jsx
@@ -1,6 +1,9 @@
 import React from "react";
 import axios from "axios";
 import { useSelector } from "react-redux";
+import { NavContainer } from "../../Components/Navbar/NavContainer";
+import styles from "./DriverDash.module.css";
+import { Button } from "@mui/material";
 
 export const DriverJobs = () => {
   const [driver, setDriver] = React.useState("");
@@ -21,7 +24,8 @@ export const DriverJobs = () => {
         jobs: [id],
       })
       .then((res) => {
-        console.log(res.data);
+        // console.log(res.data);
+        fetchUser();
       });
     return await axios
       .patch(`http://localhost:5000/package/${id}`)
@@ -34,16 +38,54 @@ export const DriverJobs = () => {
   console.log("Driver", driver);
 
   return (
-    <>
-      {driver !== "" &&
-        driver?.jobs.map((e) => {
-          return (
-            <div key={e._id}>
-              <h1>{e.packageName}</h1>;
-              <button onClick={() => declineJob(e._id)}>Decline</button>
-            </div>
-          );
-        })}
-    </>
+    <div className={styles.jobs_container}>
+      <NavContainer page="home" />
+      <div className={styles.jobs_sub_container}>
+        {driver !== "" &&
+          driver?.jobs.map((e) => {
+            // return (
+            //   <div key={e._id}>
+            //     <h1>{e.packageName}</h1>;
+            //     <button onClick={() => declineJob(e._id)}>Decline</button>
+            //   </div>
+            // );
+            return (
+              <div key={e._id} className={styles.list}>
+                <div>
+                  <div className={styles.route}>
+                    <div className={styles.text}>
+                      <span>From:</span>
+                      <span>{e?.from}</span>
+                    </div>
+                    <div className={styles.text}>
+                      <span>Destination:</span>
+                      <span>{e?.to}</span>
+                    </div>
+                  </div>
+                  <div className={styles.text}>
+                    <span>Item: </span>
+                    <span>{e?.packageName}</span>
+                  </div>
+                  <div className={styles.text}>
+                    <span>Weight:</span>
+                    <span>{e?.weight}</span>
+                  </div>
+                </div>
+                <div className={styles.image}>
+                  <img src={e?.image} alt="" />
+                </div>
+                <Button
+                  variant="contained"
+                  onClick={() => {
+                    declineJob(e?._id);
+                  }}
+                >
+                  Decline
+                </Button>
+              </div>
+            );
+          })}
+      </div>
+    </div>
   );
 };
